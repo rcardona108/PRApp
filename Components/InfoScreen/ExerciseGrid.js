83c409a23bdf9f5978d43bb9631a9820d16955b3
dgoldstein2 <dgoldstein0406@gmail.com>
2023-03-23 14:05:50 -0400

    Update ExerciseGrid.js

--- Components/InfoScreen/ExerciseGrid.js
+++ Components/InfoScreen/ExerciseGrid.js
@@ -1,10 +1,20 @@
 import { StyleSheet } from "react-native";
 import { View, Text } from "react-native";
+import { Dimensions } from "react-native";
+import { useEffect, useState } from "react";
+
 const ExerciseGrid = () => {
     //shows the sets
-    <View style = {styles.container}>
+    
+    const [windowLength,setWindowLength] = useState(Dimensions.get('window'));
+    /*useEffect(()=>{
+        setWindowLength(Dimensions.get('window'));
+    },[]);
+    */
+    return(
+    <View style = {[styles.container,{width: windowLength}]}>
         <View style = {styles.lines}>
-            <Text>
+            <Text style = {{color:'white'}}>
                 LOL
             </Text>
         </View>
@@ -15,35 +25,34 @@ const ExerciseGrid = () => {
         <View style = {[styles.lines, {transform:[{translateX: 200}]}]}>
         
         </View>
-        <View style = {[styles.lines, {transform:[{translateY: 100}]}]}>
         
-        </View>
         </View>
         <View style = {styles.lines}>
-        <View style = {[styles.lines, {transform:[{translateY: 100}]}]}>
         
-        </View> 
         </View>
         
 
     </View>
+    );
 };
 const styles = StyleSheet.create({
     container:{
+        borderColor: 'white',
         borderWidth: 5,
         height: 200,
-        width: 400,
         borderRadius: 30,
+       
 
     },
     lines:{
-        //backgroundColor:
+        backgroundColor:'white',
         height: 194,
         width: 3,
         position:'absolute',
         transform: [
             {translateX: 100}
         ],
+        
         
     }
 
