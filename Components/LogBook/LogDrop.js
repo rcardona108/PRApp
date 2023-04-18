650974acb80f43057bb0707b45cd60dea88d1ac5
rcardona108 <radcardona@gmail.com>
2023-04-18 13:52:59 -0400

    pulling document from firestore

--- Components/LogBook/LogDrop.js
+++ Components/LogBook/LogDrop.js
@@ -5,11 +5,11 @@ import Accordion from './Accordion';
 import { async } from '@firebase/util';
 import { useEffect } from 'react';
 export default LogDrop =async () => {
-        const colRef = collection(db, "UsersData");
-        const docRef = doc(colRef, "3-14-2023-1");
-        const docSnap = await getDoc(docRef);
-        useEffect(()=>{
-            console.log(docSnap[1].data())
+    const colRef = collection(db, "UsersData");
+    const docRef = doc(colRef, "3-14-2023-1");
+        useEffect(async()=>{
+            const docSnap = await getDoc(docRef);
+            console.log(docSnap.data())
         },[])
          return(
             <ScrollView>
